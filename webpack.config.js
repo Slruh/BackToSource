b1b25251cdd23929b1c1a50ffc22398d1d1d25a8
Ryan Hurley <rhurley@twitter.com>
2020-10-20 09:58:30 -0400

    Added typescript to webpack but have not tested any typescript features

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,12 @@ var webpack = require("webpack"),
   path = require("path"),
   CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin,
   CopyWebpackPlugin = require("copy-webpack-plugin"),
-  HtmlWebpackPlugin = require("html-webpack-plugin"),
   WriteFilePlugin = require("write-file-webpack-plugin");
 
 var options = {
   // Use the content.js file as an entry point 
   entry: {
-    content: path.join(__dirname, "src", "js", "content.js"),
+    content: path.join(__dirname, "src", "js", "content.ts"),
   },
   mode: process.env.NODE_ENV || "development",
   // Write output to a build folder
@@ -16,7 +15,15 @@ var options = {
     path: path.join(__dirname, "build"),
     filename: "[name].bundle.js"
   },
-
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.ts?$/,
+        use: 'awesome-typescript-loader?{configFileName: "tsconfig.json"}',
+      },
+    ],
+  },
   plugins: [
     // clean the build folder
     new CleanWebpackPlugin(),
